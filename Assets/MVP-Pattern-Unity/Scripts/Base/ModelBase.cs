04d1cfe86e5b66bfeea1b6369115125904584181
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 00:53:28 +0900

    Feat: IModel 인터페이스 상속 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public abstract class ModelBase<T> : MonoBehaviour
+public abstract class ModelBase<T> : MonoBehaviour, IModel<T>
 {
-    
+    public T Data { get; private set; }
 }
