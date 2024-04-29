5c584d7b4cc4b1f583cabbda40fde694335f4bd7
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 00:49:38 +0900

    Add: TriggerDataChange 메서드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
@@ -5,4 +5,5 @@ public interface IModel<T>
     void AddListener(UnityAction<T> listener);
     void RemoveListener(UnityAction<T> listener);
     void ClearListener();
+    void TriggerDataChange(T data);
 }
