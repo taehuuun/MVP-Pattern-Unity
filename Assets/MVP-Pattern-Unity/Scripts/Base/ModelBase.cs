8637a1dac782c6c74044417ef5f4d77249588b2f
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 00:53:49 +0900

    Add: Data 프로퍼티 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public abstract class ModelBase<T> : MonoBehaviour, IModel<T>
 {
+    public T Data { get; private set; }
     
+    private readonly UnityEvent<T> _onDataUpdated = new();
+    
+    public virtual void AddListener(UnityAction<T> listener)
+    {
+        _onDataUpdated.AddListener(listener);
+    }
+
+    public virtual void RemoveListener(UnityAction<T> listener)
+    {
+        _onDataUpdated.RemoveListener(listener);
+    }
+
+    public virtual void ClearListener()
+    {
+        _onDataUpdated.RemoveAllListeners();
+    }
+
+    public virtual void TriggerDataChange(T data)
+    {
+        _onDataUpdated?.Invoke(data);
+    }
 }
