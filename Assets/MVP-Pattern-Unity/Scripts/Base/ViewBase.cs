2355d483ec4f3f1c942b73ae4ceb675c54802d09
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 20:12:15 +0900

    Feat: Start 메서드
    Initialize 메서드 호출하는 로직 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
@@ -6,6 +6,11 @@ public abstract class ViewBase<T> : MonoBehaviour, IView<T>
 {
     private readonly Dictionary<string, UnityEventBase> _events = new();
 
+    private void Start()
+    {
+        Initialize();
+    }
+
     protected abstract void Initialize();
     
     public abstract void UpdateView(T data);
