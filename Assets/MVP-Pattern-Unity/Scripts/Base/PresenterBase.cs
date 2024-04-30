65b623361b0cc1f55845f4aba30e063f5098885b
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:11:28 +0900

    Feat: 제네릭 타입 키워드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public abstract class PresenterBase : MonoBehaviour, IPresenter
+public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
 {
+    [SerializeField] private ModelBase<T> model;
+    [SerializeField] private ViewBase<T> view;
+
+    protected void Start()
+    {
+        Initialize();
+        
+        AddViewListeners();
+    }
+
+    protected void OnDestroy()
+    {
+        model.ClearListener();
+    }
+
+    public virtual void Initialize()
+    {
+        model.AddListener(HandleModelUpdate);
+    }
 
 }
