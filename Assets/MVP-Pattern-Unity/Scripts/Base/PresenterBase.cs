debee649f7985a6eb3de8623f5e512b6ef884b12
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:51:44 +0900

    Feat: HandleModelUpdate 메서드 구현
    model의 데이터가 업데이트 될 때 트리거되는 메서드

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -5,4 +5,10 @@ public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
     [SerializeField] private ModelBase<T> model;
     [SerializeField] private ViewBase<T> view;
 
+
+
+    protected virtual void HandleModelUpdate(T data)
+    {
+        view.UpdateView(data);
+    }
 }
