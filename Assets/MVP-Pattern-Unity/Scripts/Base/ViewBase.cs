375e01545fbd93a3ee8c5d05a4fe55e94f2df8d2
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 20:10:18 +0900

    Refactor: Initialized 메서드 명 Initialize로 변경

--- Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
@@ -6,7 +6,7 @@ public abstract class ViewBase<T> : MonoBehaviour, IView<T>
 {
     private readonly Dictionary<string, UnityEventBase> _events = new();
 
-    protected abstract void Initialized();
+    protected abstract void Initialize();
     
     public abstract void UpdateView(T data);
 }
