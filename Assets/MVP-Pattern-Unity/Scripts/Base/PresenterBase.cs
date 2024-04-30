a84373449c6f8972224859fff513621526740479
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:53:00 +0900

    Feat: HideView 메서드 구현
    view를 비활성화 하는 메서드

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -5,7 +5,10 @@ public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
     [SerializeField] private ModelBase<T> model;
     [SerializeField] private ViewBase<T> view;
 
-
+    public virtual void HideView()
+    {
+        view.gameObject.SetActive(false);
+    }
 
     protected virtual void HandleModelUpdate(T data)
     {
