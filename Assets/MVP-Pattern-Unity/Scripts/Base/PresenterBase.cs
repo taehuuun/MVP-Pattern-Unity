71b43a9703ae20ab82c3fd4b8db76a8dc2c749a9
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:40:45 +0900

    Add: view 필드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
 {
     [SerializeField] private ModelBase<T> model;
+    [SerializeField] private ViewBase<T> view;
 
 }
