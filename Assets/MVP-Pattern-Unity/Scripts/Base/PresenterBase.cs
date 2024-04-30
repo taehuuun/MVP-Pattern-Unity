fb6d8d256add712bbfc67fe84bf082f3aff42809
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:53:22 +0900

    Feat: ShowView 메서드 구현
    view를 활성화 하는 메서드

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -5,6 +5,11 @@ public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
     [SerializeField] private ModelBase<T> model;
     [SerializeField] private ViewBase<T> view;
 
+    public virtual void ShowView()
+    {
+        view.gameObject.SetActive(true);
+    }
+
     public virtual void HideView()
     {
         view.gameObject.SetActive(false);
