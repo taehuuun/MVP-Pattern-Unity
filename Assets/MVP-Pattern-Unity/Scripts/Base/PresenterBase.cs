634eadb0be1f74dbdaca23a1b85eb1f1b9dd6225
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 19:19:12 +0900

    Revert "Feat: 제네릭 타입 키워드 추가"
    This reverts commit 65b623361b0cc1f55845f4aba30e063f5098885b.

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -1,25 +1,6 @@
 using UnityEngine;
 
-public abstract class PresenterBase<T> : MonoBehaviour, IPresenter
+public abstract class PresenterBase : MonoBehaviour, IPresenter
 {
-    [SerializeField] private ModelBase<T> model;
-    [SerializeField] private ViewBase<T> view;
-
-    protected void Start()
-    {
-        Initialize();
-        
-        AddViewListeners();
-    }
-
-    protected void OnDestroy()
-    {
-        model.ClearListener();
-    }
-
-    public virtual void Initialize()
-    {
-        model.AddListener(HandleModelUpdate);
-    }
 
 }
