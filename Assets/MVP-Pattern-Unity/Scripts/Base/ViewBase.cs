99e823b68e0b3c54613b23eed225091f9b7be555
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-04-30 20:01:20 +0900

    Add: _events 필드 추가
    View에서 발생하는 이벤트들을 관리하기 위한 필드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ViewBase.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class ViewBase<T> : MonoBehaviour, IView<T>
 {
+    private readonly Dictionary<string, UnityEventBase> _events = new();
     public abstract void UpdateView(T data);
 }
