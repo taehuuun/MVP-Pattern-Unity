4b06c1779b8d43541bb704a97b7441e535f5cfd3
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-02 18:12:12 +0900

    Add: 필요 필드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerView.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerView.cs
@@ -1,5 +1,29 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickerView : ViewBase
 {
+    [Header("Header UI")]
+    [SerializeField] private TMP_Text goldPerClickText;
+    [SerializeField] private TMP_Text goldPerSecText;
+    [SerializeField] private TMP_Text currentGoldText;
+    
+    [Header("Upgrade UI")]
+    [SerializeField] private Button goldPerClickUpgradeBtn;
+    [SerializeField] private Button goldPerSecUpgradeBtn;
+    [SerializeField] private TMP_Text nextGoldPerClickUpgradeBtnLevelText;
+    [SerializeField] private TMP_Text nextGoldPerSecUpgradeBtnLevelText;
+    [SerializeField] private TMP_Text nextGoldPerClickUpgradeBtnCostText;
+    [SerializeField] private TMP_Text nextGoldPerSecUpgradeBtnCostText;
+
+    [Header("Touch UI")]
+    [SerializeField] private Button screenBtn;
+    
+    [Header("Circle UI")]
+    [SerializeField] private Transform circle;
+
+    private readonly float _scaleUpTime = 0.2f;
+
 }
