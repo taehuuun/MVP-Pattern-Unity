02b3ff7ffb3b78c47eb47a52413f7a32ed6f9c1b
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-02 17:04:44 +0900

    Add: 필요 프로퍼티 및 필드 추가

--- Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerModel.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerModel.cs
@@ -1,4 +1,18 @@
 public class ClickerModel : ModelBase
 {
-    
 }
+
+    public PlayerData Data { get; private set; }
+    public int CurrentGoldPerClick { get; private set; }
+    public int CurrentGoldPerSec { get; private set; }
+    public int NextGoldPerClickCost => _goldPerClickUpgrade.costs[Data.goldPerClickLevel+1];
+    public int NextGoldPerSecCost => _goldPerSecUpgrade.costs[Data.goldPerSecLevel+1];
+    public int NextGoldPerClickLevel => Data.goldPerClickLevel + 1;
+    public int NextGoldPerSecLevel => Data.goldPerSecLevel + 1;
+    
+    private Upgrade _goldPerClickUpgrade;
+    private Upgrade _goldPerSecUpgrade;
+
+    private const string _GOLD_PER_CLICK_LEVEL_KEY = "ClickerGoldPerClickLevel";
+    private const string _GOLD_PER_SEC_LEVEL_KEY = "ClickerGoldPerSecLevel";
+    private const string _GOLD_KEY = "ClickerGold";
