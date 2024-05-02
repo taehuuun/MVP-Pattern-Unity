95581d4d9a509b95a1c4be9c554e36b374db9ac2
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-03 04:07:24 +0900

    Comment: IModel 인터페이스 XML 주석 추가

--- Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
@@ -1,9 +1,16 @@
 using UnityEngine.Events;
 
+/// <summary>
+/// 모든 모델 클래스에 상속되어야 하는 인터페이스
+/// </summary>
 public interface IModel
 {
     void AddListener(UnityAction<ModelBase> listener);
     void RemoveListener(UnityAction<ModelBase> listener);
     void ClearListener();
+    
+    /// <summary>
+    /// 모델 내 등록 된 이벤트를 발생 시키는 메서드
+    /// </summary>
     void TriggerEvent();
 }
