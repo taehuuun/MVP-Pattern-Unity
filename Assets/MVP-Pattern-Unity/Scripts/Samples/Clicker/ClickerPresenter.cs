328913b8fd17b40169ecc748fcbb21dd3d685bca
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-03 01:24:35 +0900

    Feat: GetGoldPerSec 코루틴 메서드 구현
    플레이 하는 동안 매 1초 마다 model의 SecAddGold 메서드를 호출하는 코루틴 메서드 구현

--- Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerPresenter.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Samples/Clicker/ClickerPresenter.cs
@@ -1,6 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class ClickerPresenter : PresenterBase
 {
-   
+    private readonly WaitForSeconds _goldPerSecDelay = new(1f);
+    
+    private IEnumerator GetGoldPerSec()
+    {
+        while (true)
+        {
+            ((ClickerModel)model).SecAddGold();
+            yield return _goldPerSecDelay;
+        }
+    }
 }
