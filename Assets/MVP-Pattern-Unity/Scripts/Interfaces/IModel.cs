828aeac523e11c19334985f869d0d7e8cbee363a
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-04 08:12:35 +0900

    Comment: AddListener, RemoveListener 메서드 XML 주석 수정
    메서드의 매개변수 타입 변경에 따라 주석 내 param 내용 수정

--- Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Interfaces/IModel.cs
@@ -8,13 +8,13 @@ public interface IModel
     /// <summary>
     /// 모델 내 이벤트에 리스너를 추가하는 메서드
     /// </summary>
-    /// <param name="listener">추가 할 리스너(ModelBase)</param>
+    /// <param name="listener">추가 할 리스너</param>
     void AddListener(UnityAction listener);
     
     /// <summary>
     /// 모델 내 등록 된 이벤트 리스너를 제거하는 메서드
     /// </summary>
-    /// <param name="listener">제거 할 리스너(ModelBase)</param>
+    /// <param name="listener">제거 할 리스너</param>
     void RemoveListener(UnityAction listener);
     
     /// <summary>
