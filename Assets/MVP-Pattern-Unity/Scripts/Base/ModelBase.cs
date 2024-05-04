8955dad68fac7a4b088657bd123c0f693e917a04
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2024-05-04 12:24:23 +0900

    Remove: IModel 인터페이스 파일 제거
    해당 인터페이스를 구현하는 클래스가 ModelBase 외에 존재하지 않기에 파일 제거 및 ModelBase 메서드 주석 수정

--- Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/ModelBase.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 /// <summary>
 /// IModel 인터페이스를 상속 받는 모든 모델의 부모 추상 클래스
 /// </summary>
-public abstract class ModelBase : MonoBehaviour, IModel
+public abstract class ModelBase : MonoBehaviour
 {
     private readonly UnityEvent _onModelChanged = new();     // 모델 변경 시 발생하는 이벤트
 
@@ -16,25 +16,35 @@ public abstract class ModelBase : MonoBehaviour, IModel
         
     }
     
-    /// <inheritdoc cref="IModel.AddListener"/>
+    /// <summary>
+    /// 모델 내 이벤트에 리스너를 추가하는 메서드
+    /// </summary>
+    /// <param name="listener">추가 할 리스너</param>
     public void AddListener(UnityAction listener)
     {
         _onModelChanged.AddListener(listener);
     }
 
-    /// <inheritdoc cref="IModel.RemoveListener"/>
+    /// <summary>
+    /// 모델 내 등록 된 이벤트 리스너를 제거하는 메서드
+    /// </summary>
+    /// <param name="listener">제거 할 리스너</param>
     public void RemoveListener(UnityAction listener)
     {
         _onModelChanged.RemoveListener(listener);
     }
 
-    /// <inheritdoc cref="IModel.ClearListener"/>
+    /// <summary>
+    /// 모델 내 등록 된 모든 리스너를 제거하는 메서드
+    /// </summary>
     public void ClearListener()
     {
         _onModelChanged.RemoveAllListeners();
     }
 
-    /// <inheritdoc cref="IModel.TriggerEvent"/>
+    /// <summary>
+    /// 모델 내 등록 된 이벤트를 발생 시키는 메서드
+    /// </summary>
     public void TriggerEvent()
     {
         _onModelChanged?.Invoke();
