046b3b874a42f6ba3e3d10157d7a973772051474
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2025-02-24 03:29:52 +0900

    Package: TextMeshPro 패키지 업데이트

---
+++ Assets/MVP-Pattern-Unity/Scripts/Base/BindableModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public abstract class BindableModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return;
+        }
+        
+        field = value;
+        OnPropertyChanged(propertyName);
+    }
+}
