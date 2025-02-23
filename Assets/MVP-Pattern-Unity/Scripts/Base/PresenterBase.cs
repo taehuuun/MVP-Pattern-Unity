eae46619858116c7d5f42e2da6ab4782061a5ced
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2025-02-24 07:02:31 +0900

    Refactor: PresenterBase 코드 구조 개선
    필드를 private로 변경하고 PropertyChanged를 활용하는 방식으로 리팩토링을 진행했습니다. 불필요한 이벤트 관련 메서드를 제거하고 가독성을 향상시켰습니다.

--- Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
+++ Assets/MVP-Pattern-Unity/Scripts/Base/PresenterBase.cs
@@ -1,33 +1,33 @@
-using System.Collections.Generic;
+using System;
+using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.Events;
 
 /// <summary>
 /// IPresenter 인터페이스를 상속 받는 모든 Presenter의 부모 추상 클래스
 /// </summary>
-public abstract class PresenterBase<TModel> : MonoBehaviour where TModel : ModelBase 
+public abstract class PresenterBase<TModel> : MonoBehaviour, IPresenter where TModel : ModelBase 
 {
-    // View의 상호작용 이벤트를 관리하는 딕셔너리 필드
-    public Dictionary<string, UnityEventBase> Events { get; }= new();
-    
-    [SerializeField] protected TModel model;                // ModelBase를 상속 받는 model 필드
-    [SerializeField] protected ViewBase<TModel> view;       // ViewBase를 상속 받는 view 필드
+    private TModel _model;                // ModelBase를 상속 받는 model 필드
+    private ViewBase<TModel> _view;       // ViewBase를 상속 받는 view 필드
 
     /// <summary>
     /// 최초 활성화 시 호출되는 메서드
     /// </summary>
     protected void Start()
     {
-        Initialize();
-    }
+        if (TryGetComponent(out _model) == false)
+        {
+            Debug.LogError("Model이 null 입니다");
+            return;
+        }
 
-    /// <summary>
-    /// 오브젝트 파괴 시 호출되는 메서드
-    /// </summary>
-    protected virtual void OnDestroy()
-    {
-        model.ClearListener();
-        RemoveAllEvent();
+        if (TryGetComponent(out _view) == false)
+        {
+            Debug.LogError("View가 null 입니다");
+            return;
+        }
+        
+        Initialize();
     }
 
     /// <summary>
@@ -35,155 +35,57 @@ public abstract class PresenterBase<TModel> : MonoBehaviour where TModel : Model
     /// </summary>
     protected virtual void Initialize()
     {
-        model.Initialize();
-        view.Initialize(this);
+        _model.PropertyChanged += OnModelPropertyChanged;
         
-        model.AddListener(HandleModelUpdate);
-        
-        model.TriggerEvent();
-    }
-
-    /// <summary>
-    /// eventName에 해당하는 이벤트에 리스너를 추가 시키는 메서드
-    /// </summary>
-    /// <param name="eventName">이벤트 명</param>
-    /// <param name="listener">추가 할 리스너</param>
-    protected virtual void AddListener(string eventName, UnityAction listener)
-    {
-        if (!Events.ContainsKey(eventName))
-        {
-            Events.Add(eventName, new UnityEvent());
-        }
-
-        ((UnityEvent)Events[eventName]).AddListener(listener);
-    }
-
-    /// <summary>
-    /// eventName에 해당하는 이벤트에 인자를 1개 이상 가지는 리스너를 추가 시키는 메서드
-    /// </summary>
-    /// <param name="eventName">이벤트 명</param>
-    /// <param name="listener">추가 할 리스너</param>
-    protected virtual void AddListener(string eventName, UnityAction<object[]> listener)
-    {
-        if (!Events.ContainsKey(eventName))
-        {
-            Events.Add(eventName, new UnityEvent<object[]>());
-        }
-
-        ((UnityEvent<object[]>)Events[eventName]).AddListener(listener);
-    }
-
-    /// <summary>
-    /// eventName에 해당하는 이벤트의 리스너를 제거하는 메서드
-    /// </summary>
-    /// <param name="eventName">이벤트 명</param>
-    /// <param name="listener">제거 할 리스너</param>
-    protected virtual void RemoveListener(string eventName, UnityAction listener)
-    {
-        if (Events.TryGetValue(eventName, out var unityEventBase))
-        {
-            if (unityEventBase is UnityEvent unityEvent)
-            {
-                unityEvent.RemoveListener(listener);
-            }
-        }
-    }
-
-    /// <summary>
-    /// eventName에 해당하는 이벤트의 인자를 1개 이상 가지는 리스너를 제거하는 메서드
-    /// </summary>
-    /// <param name="eventName">이벤트 명</param>
-    /// <param name="listener">제거 할 리스너</param>
-    protected virtual void RemoveListener(string eventName, UnityAction<object[]> listener)
-    {
-        if (Events.TryGetValue(eventName, out var unityEventBase))
-        {
-            if (unityEventBase is UnityEvent<object[]> unityEvent)
-            {
-                unityEvent.RemoveListener(listener);
-            }
-        }
-    }
-
-    /// <summary>
-    /// eventName에 해당하는 이벤트를 발생 시키는 메서드
-    /// </summary>
-    /// <param name="eventName">발생 시킬 이벤트 명</param>
-    public virtual void TriggerEvent(string eventName)
-    {
-        if (Events.TryGetValue(eventName, out var unityEventBase))
-        {
-            if (unityEventBase is UnityEvent unityEvent)
-            {
-                unityEvent.Invoke();
-            }
-        }
+        _view.Initialize(this);
+        _model.Initialize();
     }
     
     /// <summary>
-    /// eventName에 해당하는 인자를 1개 이상 가지는 이벤트를 트리거 시키는 메서드
-    /// </summary>
-    /// <param name="eventName">발생 시킬 이벤트 명</param>
-    /// <param name="args">이벤트 발생 시 전달되는 인자 배열</param>
-    public virtual void TriggerEvent(string eventName, object[] args)
-    {
-        if (Events.TryGetValue(eventName, out var unityEventBase))
-        {
-            if (unityEventBase is UnityEvent<object[]> unityEvent)
-            {
-                unityEvent.Invoke(args);
-            }
-        }
-    }
-    
-    /// <summary>
-    /// eventName에 해당하는 이벤트를 제거하는 메서드
-    /// </summary>
-    /// <param name="eventName">제거 할 이벤트 명</param>
-    protected virtual void RemoveEvent(string eventName)
-    {
-        if (Events.TryGetValue(eventName, out var unityEventBase))
-        {
-            unityEventBase.RemoveAllListeners();
-        }
-        
-        Events.Remove(eventName);
-    }
-
-    /// <summary>
-    /// _events 딕셔너리의 모든 이벤트 및 리스너를 제거하는 메서드
-    /// </summary>
-    protected virtual void RemoveAllEvent()
-    {
-        foreach (var events in Events.Values)
-        {
-            events.RemoveAllListeners();
-        }
-
-        Events.Clear();
-    }
-
-    /// <summary>
-    /// View를 활성화 시키는 메서드
+    /// View의 ShowView 메서드를 호출하는 메서드
     /// </summary>
     public virtual void ShowView()
     {
-        view.gameObject.SetActive(true);
+        if (_view == null)
+        {
+            Debug.LogWarning("[PresenterBase] View를 활성화 할 수 없음, View가 Null임");
+            return;
+        }
+        
+        _view.ShowView();
     }
 
     /// <summary>
-    /// View를 비활성화 시키는 메서드
+    /// View를 HideView 메서드를 호출하는 메서드
     /// </summary>
     public virtual void HideView()
     {
-        view.gameObject.SetActive(false);
+        if (_view == null)
+        { 
+            Debug.LogWarning("[PresenterBase] View를 비활성화 할 수 없음, View가 Null임");
+            return;
+        }
+        
+        _view.HideView();
     }
 
-    /// <summary>
-    /// 모델이 변경될 때 호출 되는 메서드
-    /// </summary>
-    protected virtual void HandleModelUpdate()
+    public TModel GetModel() => _model;
+
+    public void InvokeMethod(Enum methodType)
     {
-        view.UpdateView(model);
+        if (_model == null)
+        {
+            Debug.LogError("[PresenterBase] Model 내 메서드 호출 실패, Model이 Null임");
+            return;
+        }
+
+        _model.InvokeMethod(methodType);
+    }
+    
+    protected void ForceUpdateView(string propertyName) => _view.UpdateView(propertyName);
+
+    private void OnModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        _view.UpdateView(e.PropertyName);
     }
 }
